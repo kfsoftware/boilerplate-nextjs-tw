cdc5d6d6d8f67a7fc2345ba2ba6ffdc05f1b8aee
David VIEJO <dviejo@kungfusoftware.es>
2024-08-06 12:06:59 +0200

    Fix tailwind
    Signed-off-by: David VIEJO <dviejo@kungfusoftware.es>

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,9 +2,18 @@
 
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
+import path from "path";
 
+const content = [
+  path.join(path.dirname(require.resolve('@repo/ui')), '**/*.{ts,tsx}'),
+  '../../packages/ui/src/components/**/*.{ts.tsx}',
+  './pages/**/*.{ts,tsx}',
+  './components/**/*.{ts,tsx}',
+  './app/**/*.{ts,tsx}',
+  './src/**/*.{ts,tsx}',
+]
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx"],
+  content,
   presets: [sharedConfig],
 };
 
